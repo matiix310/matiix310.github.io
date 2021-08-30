c04a43049593b2041b975856b3cbad82a17412c2
matiix310 <lucas.stephan.12@outlook.fr>
2021-08-30 16:56:55 +0200

    ''

--- script.js
+++ script.js
@@ -25,28 +25,40 @@ startBtn.addEventListener('click', function() {
 
 // interval pour le fond de la section presentation
 const presSection = document.getElementById('presentation');
-const meteorBig = '<svg id="meteorINDEX" class="meteor-big" style="left:Xpos;top:Ypos;" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1920 1080" style="enable-background:new 0 0 1920 1080;" xml:space="preserve"><path d="M600.5,294.5c0,0,700.89,220.11,657.45,301.21C1214.5,676.8,600.5,294.5,600.5,294.5z"/></svg>';
+const meteorBig = '<svg id="meteorINDEX" class="meteor-big meteor" style="left:Xpos;top:Ypos;" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1920 1080" style="enable-background:new 0 0 1920 1080;" xml:space="preserve"><path d="M600.5,294.5c0,0,700.89,220.11,657.45,301.21C1214.5,676.8,600.5,294.5,600.5,294.5z"/></svg>';
+const meteorMid = '<svg id="meteorINDEX" class="meteor-mid meteor" style="left:Xpos;top:Ypos;" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1920 1080" style="enable-background:new 0 0 1920 1080;" xml:space="preserve"><path d="M1145.5,305.5c0,0,372.01,157.21,357,196C1490.5,532.5,1145.5,305.5,1145.5,305.5z"/></svg>';
+const meteorSmall = '<svg id="meteorINDEX" class="meteor-small meteor" style="left:Xpos;top:Ypos;" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1920 1080" style="enable-background:new 0 0 1920 1080;" xml:space="preserve"><path d="M433.45,363.3c0,0,275.87,173.26,256.05,205.2C671.5,597.5,433.45,363.3,433.45,363.3z"/></svg>';
 
 var index = 0;
 var x = setInterval(() => {
-    var tempIndex = index;
     index = index+1;
+    var tempIndex = index;
+    let meteor;
+
+    // taille de la météorite
+    let size = Math.round(Math.random()*2);
+    switch (size) {
+        case 0:
+            meteor = meteorBig;
+            break;
+        case 1:
+            meteor = meteorMid;
+            break;
+        case 2:
+            meteor = meteorSmall;
+            break;
+        default:
+            meteor = meteorBig;
+    }
+
+    console.log(size);
+
     let x = Math.round(Math.random()*window.innerWidth)-100;
     let y = Math.round(Math.random()*window.innerHeight)-100;
-    let moveX = Math.round(window.innerWidth)+100;
-    let moveY = Math.round(window.innerHeight)+100;
-    presSection.innerHTML = meteorBig.replace('Xpos', x).replace('Ypos', y).replace('INDEX', tempIndex) + presSection.innerHTML;
-    setTimeout(() => {
-        document.getElementById('meteor'+tempIndex).classList.add('svg-complete');
-    }, 100);
-    setTimeout(() => {
-        document.getElementById('meteor'+tempIndex).style.fill = "yellow";
-    }, 1000);
-    setTimeout(() => {
-        document.getElementById('meteor'+tempIndex).style.transform = 'translate('+moveX+'px, '+moveY+'px)';
-    }, 1300);
+    presSection.insertAdjacentHTML( 'beforeend', meteor.replace('Xpos', x).replace('Ypos', y).replace('INDEX', tempIndex));
+
 
     setTimeout(() => {
         document.getElementById('meteor'+tempIndex).remove()
-    }, 3000);
+    }, 2500);
 }, 1000);
